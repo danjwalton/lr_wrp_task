50e9eb74c665712c4ce02b99a2ec7b0004da92be
Dan Walton <20973540+danjwalton@users.noreply.github.com>
2026-09-17 17:15:13 +0100

    Robust validation

--- script/wrp_validate.R
+++ script/wrp_validate.R
@@ -17,9 +17,6 @@ schema_col <- cbind(var = names(schema$columns), schema_col)
 schema_col[, r_type := "numeric"]
 schema_col[type == "string", r_type := "character"]
 
-#Age is incorrectly schema as string
-schema_col[var == "Age", r_type := "numeric"]
-
 #Coerce raw data to class
 vars <- schema_col$var
 wrp4[, (vars) := Map(as, .SD, schema_col$r_type), .SDcols = vars]      
@@ -34,12 +31,25 @@ schema_labs <- setNames(lapply(schema_labs$labs.V1, names), schema_labs$var)
 #Disallow ages below 15 and above 99
 schema_labs <- c(schema_labs, Age = list(15:99))
 
-#Check if constrained responses validate 
+#Return list of responses not in schema, by var
 constrained_vars <- names(schema_labs)
-wrp4[, (constrained_vars) := Map(function(var, labs) fifelse(var %in% c(labs, 9999), var, NA), .SD, schema_labs), .SDcols = constrained_vars]
+bad_vals <- rbindlist(lapply(constrained_vars, function(x) data.table(x, wrp4[[x]][!is.na(wrp4[[x]]) & !(wrp4[[x]] %in% c(schema_labs[[x]], 9999))])))
 
+if(nrow(bad_vals) > 0){
+  bad_vals_tab <- bad_vals[, .N, by = .(var = x, val = V2)][order(-N)]
+  message(sum(bad_vals_tab$N), " bad values detected.")
+}
+
+### FIXES
 #Remove the Manics references
 wrp4[wrp4 == "If You Tolerate This Your Children Will Be Next"] <- NA
+
+#Correct age in schema as string
+schema_col[var == "Age", r_type := "numeric"]
+wrp4[, Age := as.numeric(Age)]
+
+#Replace non-allowed values with NA
+wrp4[, (constrained_vars) := Map(function(var, labs) fifelse(var %in% c(labs, 9999), var, NA), .SD, schema_labs), .SDcols = constrained_vars]
 
 #Unvalidated response rows
 wrp4_fail <- wrp4[!complete.cases(wrp4)]
@@ -53,3 +63,4 @@ setwd("..")
 if(!dir.exists("clean_src")) dir.create("clean_src")
 fwrite(wrp4_validate, "clean_src/wrp4_validate.csv")
 fwrite(wrp4_fail, "clean_src/wrp4_fail.csv")
+if(exists("bad_vals_tab")) fwrite(bad_vals_tab, "clean_src/bad_vals.csv")
